69bde97ec0324cd6bf93bf0e486bcb1fb0d26bf7
saurmo - Santiago Urrego Morales <santimo0108@gmail.com>
2020-04-14 07:24:38 -0500

    CRUD personas

--- backend/03 covid-19 api/controllers/personas.js
+++ backend/03 covid-19 api/controllers/personas.js
@@ -10,11 +10,11 @@ const ServicioPg = require("../services/postgres");
  * Validar la información de la persona
  * @param {*} persona Json de la persona
  */
-let validarPersona = persona => {
+let validarPersona = (persona) => {
   if (!persona) {
     throw {
       ok: false,
-      mensaje: "La información de la persona es obligatoria."
+      mensaje: "La información de la persona es obligatoria.",
     };
   }
 
@@ -27,7 +27,7 @@ let validarPersona = persona => {
  * Guardar en base de datos una persona
  * @param {*} persona
  */
-let guardarPersona = async persona => {
+let guardarPersona = async (persona) => {
   let _servicio = new ServicioPg();
   let sql = `INSERT INTO public.personas(
               tipo_documento, documento, nombre, celular, correo)
@@ -41,11 +41,63 @@ let guardarPersona = async persona => {
   return respuesta;
 };
 
-let consultarPersonas = async () => {
+/**
+ * Consultar personas
+ * @param {*} filtros
+ */
+let consultarPersonas = async (filtros) => {
   let _servicio = new ServicioPg();
-  let sql = `SELECT * FROM personas`;
+  // { tipo_documento: 'CC' }
+  let _where = "";
+  let llaves = Object.keys(filtros);
+  llaves.forEach((x, i) => {
+    _where += `${x}='${filtros[x]}'`;
+    // _where += x + "=" + filtros[x];
+    if (i < llaves.length - 1) _where += " AND ";
+  });
+
+  let sql = `SELECT * FROM personas WHERE ${_where}`;
+  console.log(sql);
+
   let respuesta = await _servicio.ejecutarSql(sql);
   return respuesta;
 };
 
-module.exports = { validarPersona, guardarPersona, consultarPersonas };
+let consultarPersona = async (id) => {
+  let _servicio = new ServicioPg();
+  let sql = `SELECT * FROM personas WHERE documento='${id}'`;
+  let respuesta = await _servicio.ejecutarSql(sql);
+  return respuesta;
+};
+
+let modificarPersona = async (persona, id) => {
+  if (persona.documento != id) {
+    throw {
+      ok: false,
+      mensaje: "El documento de la persona no correspende al enviado.",
+    };
+  }
+  let _servicio = new ServicioPg();
+  let sql = `UPDATE public.personas
+  SET tipo_documento='${persona.tipo_documento}',  nombre='${persona.nombre}', 
+  celular='${persona.celular}', correo='${persona.correo}'
+	WHERE documento='${persona.documento}';`;
+  let respuesta = await _servicio.ejecutarSql(sql);
+  return respuesta;
+};
+
+let eliminarPersona = async (id) => {
+  let _servicio = new ServicioPg();
+  let sql = `DELETE FROM personas WHERE documento='${id}'`;
+  let respuesta = await _servicio.ejecutarSql(sql);
+  return respuesta;
+};
+
+module.exports = {
+  validarPersona,
+  guardarPersona,
+  consultarPersonas,
+  eliminarPersona,
+  consultarPersona,
+  modificarPersona,
+};
--- backend/03 covid-19 api/routes/personas.js
+++ backend/03 covid-19 api/routes/personas.js
@@ -7,13 +7,33 @@ const _controlador = require("../controllers/personas");
  * Obtener todas las personas
  */
 router.get("/personas", (req, res) => {
+  let filtros = req.query;
+
   _controlador
-    .consultarPersonas()
-    .then(respuestaDB => {
+    .consultarPersonas(filtros)
+    .then((respuestaDB) => {
       let registros = respuestaDB.rows;
       res.send({ ok: true, info: registros, mensaje: "Personas consultadas" });
     })
-    .catch(error => {
+    .catch((error) => {
+      res.send(error);
+    });
+});
+
+/**
+ * Consultar una personas por documento
+ */
+router.get("/personas/:id", (req, res) => {
+  let id = req.params.id;
+  _controlador
+    .consultarPersona(id)
+    .then((respuestaDB) => {
+      let registros = respuestaDB.rows;
+      let mensaje =
+        registros.length > 0 ? "Persona consultada." : "Sin registro.";
+      res.send({ ok: true, info: registros, mensaje });
+    })
+    .catch((error) => {
       res.send(error);
     });
 });
@@ -32,10 +52,10 @@ router.post("/personas", (req, res) => {
     // Guardar la persona en base de datos
     _controlador
       .guardarPersona(info_persona)
-      .then(respuestaDB => {
+      .then((respuestaDB) => {
         res.send({ ok: true, mensaje: "Persona guardada", info: info_persona });
       })
-      .catch(error => {
+      .catch((error) => {
         res.send(error);
       });
 
@@ -43,6 +63,39 @@ router.post("/personas", (req, res) => {
   } catch (error) {
     res.send(error);
   }
+});
+
+/**
+ * Modificar una persona
+ */
+router.put("/personas/:id", (req, res) => {
+  // Capturar el parámetro de la ruta
+  let id = req.params.id;
+
+  let persona = req.body;
+  _controlador
+    .modificarPersona(persona, id)
+    .then((respuestaDB) => {
+      res.send({ ok: true, mensaje: "Persona modificada", info: respuestaDB });
+    })
+    .catch((error) => {
+      res.send(error);
+    });
+});
+
+/**
+ * Eliminar una persona
+ */
+router.delete("/personas/:id", (req, res) => {
+  let id = req.params.id;
+  _controlador
+    .eliminarPersona(id)
+    .then((respuestaDB) => {
+      res.send({ ok: true, info: {}, mensaje: "Persona elimina" });
+    })
+    .catch((error) => {
+      res.send(error);
+    });
 });
 
 module.exports = router;
